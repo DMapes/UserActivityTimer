8d7431132b68eec82593c601d1f54c2bfc2811eb
Dan Mapes <27016732+DMapes@users.noreply.github.com>
2020-05-24 21:17:11 -0500

    Update activity_monitor_v1.2.py

--- activity_monitor_v1.2.py
+++ activity_monitor_v1.2.py
@@ -2,9 +2,9 @@ from pynput import mouse, keyboard
 import csv
 import datetime
 import os
-import threading
+import concurrent.futures
 
-delay = 15
+delay = 16
 
 log_in = os.getlogin()
 
@@ -18,20 +18,50 @@ print(f'Start Script time {script_start_time}')
 total_time_list = []
 
 
-def track_time(start_time):
-    print(f'*****Start time**** {start_time}')
+def ms_track_time(start_time):
+    print(f'*****Mouse Start time***** {start_time}')
     report_info['Last_Start'] = f'{start_time:%Y.%m.%d %I:%M:%S %p}'
+    mouse_listener = mouse.Listener()
+    mouse_listener.start()
+    mouse_listener.wait()
+    print(f'Listening for mouse {mouse_listener.native_id}')
     movement = True
     while movement is True:
-        with mouse.Events() as mouse_events, keyboard.Events() as keyboard_events:
-            ms_event = mouse_events.get(timeout=delay/2)
-            kb_event = keyboard_events.get(timeout=delay/2)
-            if (ms_event is None) and (kb_event is None):
-                print(f'You did not interact within {delay} seconds')
+        with mouse.Events() as mouse_events:
+            ms_event = mouse_events.get(timeout=delay)
+            if ms_event is None:
+                print(f'You did not interact with the mouse within {delay} seconds')
                 end_time = datetime.datetime.now()
                 print(f'End Time {end_time}')
                 mouse_listener.stop()
                 print(f'Stopped Listening to Mouse {mouse_listener.native_id}')
+                movement = False
+            else:
+                event_time = datetime.datetime.now()
+                if ms_event:
+                    print(f'Received Mouse event {event_time}')
+                movement = True
+                continue
+        time_elapsed = (end_time - start_time) - datetime.timedelta(seconds=delay)
+        print(f'Time Elapsed {time_elapsed}')
+        # add_time(time_elapsed)
+        return time_elapsed
+
+def kb_track_time(start_time):
+    print(f'*****Keyboard Start time***** {start_time}')
+    report_info['Last_Start'] = f'{start_time:%Y.%m.%d %I:%M:%S %p}'
+    keyboard_listener = keyboard.Listener()
+    keyboard_listener.start()
+    keyboard_listener.wait()
+    print(f'Listening for keyboard {keyboard_listener.native_id}')
+    movement = True
+    while movement is True:
+        with keyboard.Events() as keyboard_events:
+            kb_event = keyboard_events.get(timeout=delay)
+            if kb_event is None:
+                print(f'You did not interact with the keyboard within {delay} seconds')
+                end_time = datetime.datetime.now()
+                print(f'End Time {end_time}')
                 keyboard_listener.stop()
                 print(f'Stopped Listening to Keyboard {keyboard_listener.native_id}')
                 movement = False
@@ -39,16 +69,15 @@ def track_time(start_time):
                 event_time = datetime.datetime.now()
                 if kb_event:
                     print(f'Received Keyboard event {event_time}')
-                if ms_event:
-                    print(f'Received Mouse event {event_time}')
                 movement = True
                 continue
         time_elapsed = (end_time - start_time) - datetime.timedelta(seconds=delay)
         print(f'Time Elapsed {time_elapsed}')
-        add_time(time_elapsed)
+        # add_time(time_elapsed)
         return time_elapsed
 
 def add_time(time_elapsed):
+    print(f'Adding Time Elapsed {time_elapsed}')
     total_time_list.append(time_elapsed)
     added_time = datetime.timedelta()
     if len(total_time_list) > 1:
@@ -63,7 +92,7 @@ def add_time(time_elapsed):
 
 def report_csv():
     path = os.path.join(
-        '{}/{} Time-{}.csv'.format(new_backup_folder, log_in, f'{script_start_time:%Y.%m.%d %I.%M.%S %p}'))
+        '{}/{} Time-{}-TEST.csv'.format(new_backup_folder, log_in, f'{script_start_time:%Y.%m.%d %I.%M.%S %p}'))
     with open(path, 'w') as csvfile:
         if len(report_info) > 2:
             spamwriter = csv.DictWriter(csvfile, report_info.keys())
@@ -85,19 +114,10 @@ report_info = {}
 report_info['Day_Start'] = f'{script_start_time:%Y.%m.%d %I:%M:%S %p}'
 new_backup_folder = backup_folder()
 while today == datetime.date.today():
-    mouse_listener = mouse.Listener()
-    keyboard_listener = keyboard.Listener()
-    mouse_listener = mouse.Listener()
-    mouse_listener.start()
-    mouse_listener.wait()
-    print(f'Listening for mouse {mouse_listener.native_id}')
-    keyboard_listener = keyboard.Listener()
-    keyboard_listener.start()
-    keyboard_listener.wait()
-    print(f'Listening for keyboard {keyboard_listener.native_id}')
-    if mouse_listener.running and keyboard_listener.running:
-        start_time = datetime.datetime.now()
-        track_time(start_time)
-        report_info['Last_Start'] = f'{start_time:%Y.%m.%d %I:%M:%S %p}'
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        mouse_ex = executor.submit(ms_track_time, datetime.datetime.now())
+        keyboard_ex = executor.submit(kb_track_time, datetime.datetime.now())
+        add_time(mouse_ex.result())
+        add_time(keyboard_ex.result())
     if len(report_info) > 2:
         report = report_csv()
